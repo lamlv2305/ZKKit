3fff144d309f2125e1e2cfd558a47bd26b4fc35e
Luong Van Lam <v.lamlv@vincommerce.com>
2017-11-14 16:18:55 +0700

    First Init

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'ZKKit' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  inhibit_all_warnings! # ignore all warnings from all pods
+
+  # Pods for ZKKit
+  pod 'Texture'
+  pod 'RxSwift', '~> 4.0'
+  pod 'RxCocoa', '~> 4.0'
+
+  target 'ZKKitTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ ZKKit/ZKKit.h
@@ -0,0 +1,19 @@
+//
+//  ZKKit.h
+//  ZKKit
+//
+//  Created by Luong Van Lam on 11/14/17.
+//  Copyright © 2017 lamlv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZKKit.
+FOUNDATION_EXPORT double ZKKitVersionNumber;
+
+//! Project version string for ZKKit.
+FOUNDATION_EXPORT const unsigned char ZKKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZKKit/PublicHeader.h>
+
+
